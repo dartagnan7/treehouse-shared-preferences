2d48c2130717eb7f89cbc17377c54d90f596878c
Ryan Sherry <rsherrycomputers@gmail.com>
2018-04-10 23:10:20 -0700

    Created text field to test how data persists for fields with id's

---
+++ app/src/main/java/com/teamtreehouse/sharedpreferencesapp/MainActivity.java
@@ -0,0 +1,15 @@
+package com.teamtreehouse.sharedpreferencesapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText mEditText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mEditText = (EditText) findViewById(R.id.editText);
+    }
+}
